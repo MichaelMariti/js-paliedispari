643a7a7979955a165d02a0c9ae3ddbd949541187
MichaelMariti <michael.mariti1@gmail.com>
2025-06-07 15:22:16 +0200

    Pari e dispari

--- pari-dispari.js
+++ pari-dispari.js
@@ -1,1 +1,33 @@
 console.log('pari-dispari')
+
+function getRandomBetween(min, max) {
+    const minCeiled = Math.ceil(min)
+    const maxFloored = Math.floor(max)
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled) 
+    
+}
+
+function ePari(num) {
+    if(num % 2 ===0){
+        return true
+    } else {
+        return false
+    }
+}
+
+const sceltaUtente = prompt('Scegli pari o dispari scrivendo "pari" o "dispari"')
+const numeroUtente = parseInt(prompt ('Inserisci un numero intero da 1 a 6'))
+const numeroPC = getRandomBetween(1, 6)
+
+console.log (sceltaUtente, numeroUtente, numeroPC)
+
+const somma = numeroUtente + numeroPC
+console.log(somma)
+
+if (ePari(somma) && sceltaUtente === 'pari') {
+    console.log('Hai vinto')
+} else if (sceltaUtente === 'dispari' && !ePari(somma)) {
+    console.log('Hai vinto')
+} else {
+    console.log ('Hai perso')
+}
